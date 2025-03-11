8d78f57146b197bf7696a44651e59a6a4b33ef85
PABLO TEIXEIRA DE BARROS <PABLO.BARROS@tjtelecom.local>
2025-03-11 16:52:35 -0300

    update

--- frontend/app/ui/components/CardChart.jsx
+++ frontend/app/ui/components/CardChart.jsx
@@ -1,7 +1,7 @@
 export default function CardChart({ Icon, colorIcon, title }) {
   return (
     <div className="border border-gray-400 p-3 text-center rounded-lg max-[400px]">
-      <div className="flex items-center justify-center gap-5 mb-10">
+      <div className="flex items-center justify-center gap-2 mb-10">
         <p className="text-lg">{title}</p>
         {Icon && <Icon color={colorIcon} size={24} />}
       </div>
