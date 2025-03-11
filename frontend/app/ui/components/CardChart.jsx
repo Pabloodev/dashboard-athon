9bacf640d96c5ba8232a8868d653b2e39a23e71a
PABLO TEIXEIRA DE BARROS <PABLO.BARROS@tjtelecom.local>
2025-03-11 13:58:23 -0300

    update

--- frontend/app/ui/components/CardChart.jsx
+++ frontend/app/ui/components/CardChart.jsx
@@ -1,13 +1,11 @@
-import { Headphones } from "lucide-react";
-
 export default function CardChart({ Icon, colorIcon, title }) {
   return (
-    <div className="border border-gray-400 p-10 text-center space-y-20 rounded-lg">
-      <div className="flex items-center gap-4">
-        <p className="text-2xl">{title}</p>
-        {Icon && <Icon color={colorIcon} />}
+    <div className="border border-gray-400 p-5 md:p-8 xl:p-5 text-center rounded-lg max-w-xs md:max-w-sm xl:max-w-md mx-auto">
+      <div className="flex items-center justify-center gap-2 xl:gap-4">
+        <p className="text-lg md:text-xl xl:text-2xl">{title}</p>
+        {Icon && <Icon color={colorIcon} size={24} />}
       </div>
-      <span className="text-5xl font-bold">82</span>
+      <span className="text-4xl md:text-5xl xl:text-6xl font-bold">82</span>
     </div>
   );
 }
