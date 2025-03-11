631c24a040547d6329aa36e1ea49ae8fd55dedec
PABLO TEIXEIRA DE BARROS <PABLO.BARROS@tjtelecom.local>
2025-03-11 14:52:59 -0300

    update

--- frontend/app/dashboard/page.jsx
+++ frontend/app/dashboard/page.jsx
@@ -5,15 +5,11 @@ export default function Page() {
   return (
     <div className="max-w-screen-2xl mx-auto px-5 lg:px-10">
       <div className="m-10">
-        <h1 className="text-2xl md:text-4xl xl:text-5xl font-medium">
-          Dashboard Geral
-        </h1>
-        <p className="text-gray-400 text-lg md:text-xl xl:text-2xl">
-          Visão geral de atendimentos
-        </p>
+      <h1 className="text-4xl font-medium">Dasboard Geral</h1>
+      <p className="text-gray-400 text-xl">Visão geral de atendimentos</p>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-5 md:gap-10 xl:gap-12">
+      <div className="flex items-center gap-10 flex-wrap">
         <CardChart Icon={Headphones} colorIcon={"#06fefa"} title={"Atendimentos em andamento"} />
         <CardChart Icon={Star} colorIcon={"#fee906"} title={"Média de avaliação"} />
         <CardChart Icon={BadgeCheck} colorIcon={"#0afe06"} title={"Total Finalizado"} />
