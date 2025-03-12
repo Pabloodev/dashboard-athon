be0903d5ae94dfd057e0ae33f7a1337b44f44400
PABLO TEIXEIRA DE BARROS <PABLO.BARROS@tjtelecom.local>
2025-03-12 12:39:17 -0300

    update

--- frontend/app/dashboard/suporte/page.jsx
+++ frontend/app/dashboard/suporte/page.jsx
@@ -9,10 +9,10 @@ export default function Page() {
         <p className="text-sky-400 text-lg">Visão geral de atendimentos</p>
       </div>
       <div className="flex items-center justify-center gap-5 flex-wrap">
-        <CardChart Icon={Headphones} colorIcon={"#06fefa"} title={"Atendimentos em andamento"} />
-        <CardChart Icon={Star} colorIcon={"#fee906"} title={"Média de avaliação"} />
-        <CardChart Icon={BadgeCheck} colorIcon={"#0afe06"} title={"Total Finalizado"} />
-        <CardChart Icon={Clock} colorIcon={"#fe6a39"} title={"Tempo Médio de Atendimento"} />
+      <CardChart Icon={Headphones} colorIcon={"#06fefa"} title={"Atendimentos em andamento"} value={236} />
+        <CardChart Icon={Star} colorIcon={"#fee906"} title={"Média de avaliação"} value={5.0}/>
+        <CardChart Icon={BadgeCheck} colorIcon={"#0afe06"} title={"Total Finalizado"} value={312}/>
+        <CardChart Icon={Clock} colorIcon={"#fe6a39"} title={"Tempo Médio de Atendimento"} value={"2,3 minutos"}/>
       </div>
     </div>
   );
--- frontend/app/ui/components/CardChart.jsx
+++ frontend/app/ui/components/CardChart.jsx
@@ -1,11 +1,11 @@
-export default function CardChart({ Icon, colorIcon, title }) {
+export default function CardChart({ Icon, colorIcon, title, value }) {
   return (
     <div className="border border-gray-400 p-3 text-center rounded-lg min-w-[200px]">
       <div className="flex items-center justify-center gap-2 mb-10">
         <p className="text-lg">{title}</p>
         {Icon && <Icon color={colorIcon} size={24} />}
       </div>
-      <span className="text-4xl font-bold">82</span>
+      <span className="text-4xl font-bold">{value}</span>
     </div>
   );
 }
