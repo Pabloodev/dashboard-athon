a316d04e07f0d9ff6e7bc323f5ac4489fc69b10a
PABLO TEIXEIRA DE BARROS <PABLO.BARROS@tjtelecom.local>
2025-03-12 12:24:34 -0300

    update

--- frontend/app/dashboard/page.jsx
+++ frontend/app/dashboard/page.jsx
@@ -3,13 +3,13 @@ import { Headphones, Star, BadgeCheck, Clock } from "lucide-react";
 
 export default function Page() {
   return (
-    <div className="max-w-screen-2xl mx-auto px-5 lg:px-10">
-      <div className="m-10">
-      <h1 className="text-2xl font-medium">Dashboard Geral</h1>
-      <p className="text-gray-400 text-lg">Visão geral de atendimentos</p>
+    <div className="">
+      <div className="">
+      <h1 className="">Dashboard Geral</h1>
+      <p className="">Visão geral de atendimentos</p>
       </div>
 
-      <div className="flex items-center justify-center gap-5 flex-wrap">
+      <div className="">
         <CardChart Icon={Headphones} colorIcon={"#06fefa"} title={"Atendimentos em andamento"} />
         <CardChart Icon={Star} colorIcon={"#fee906"} title={"Média de avaliação"} />
         <CardChart Icon={BadgeCheck} colorIcon={"#0afe06"} title={"Total Finalizado"} />
